4857d1aa8ee2136e754227e3befc4d17a28d7558
Nathan Osman <nathan@quickmediasolutions.com>
2017-09-15 20:27:15 -0700

    Finish initial design of presenter.

--- presenter/presenter.go
+++ presenter/presenter.go
@@ -1,33 +1,52 @@
 package presenter
 
 import (
-	"os"
+	"io"
+	"time"
 
 	"github.com/nathan-osman/go-ldserver/manager"
 )
 
 // Presenter runs a performance, executing each of the events in realtime.
 type Presenter struct {
-	perf performance
+	perf      performance
+	stopCh    chan bool
+	stoppedCh chan bool
 }
 
 func (p *Presenter) run() {
-	//...
+	defer close(p.stoppedCh)
+	start := time.Now()
+	for _, e := range p.perf {
+		select {
+		case <-time.After(start.Add(e.offset).Sub(time.Now())):
+			e.light.SetState(e.state)
+		case <-p.stopCh:
+			return
+		}
+	}
 }
 
-// NewPresenter creates a presenter for the specified file.
-func NewPresenter(m manager.Manager, filename string) (*Presenter, error) {
-	r, err := os.Open(filename)
+// NewPresenter creates a presenter for the specified reader.
+func NewPresenter(m manager.Manager, r io.Reader) (*Presenter, error) {
+	p, err := compile(m, r)
 	if err != nil {
 		return nil, err
 	}
-	defer r.Close()
-	perf, err := compile(m, r)
-	if err != nil {
-		return nil, err
-	}
-	p := &Presenter{
-		perf: perf,
-	}
-	return p, nil
+	return &Presenter{
+		perf:      p,
+		stopCh:    make(chan bool),
+		stoppedCh: make(chan bool),
+	}, nil
+}
+
+// Start begins the performance.
+func (p *Presenter) Start() {
+	go p.run()
+}
+
+// Stop ends the performance.
+func (p *Presenter) Stop() {
+	close(p.stopCh)
+	<-p.stoppedCh
 }
